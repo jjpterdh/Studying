34a76bd5e5158ee79dc0c07e4db7328e23eea0d8
Danho <danho011185@gmail.com>
2022-04-19 03:29:32 +0900

    Initial Commit

---
+++ Studying/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Studying
+//
+//  Created by 정단호 on 2022/04/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Studying/StudyingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyingApp.swift
+//  Studying
+//
+//  Created by 정단호 on 2022/04/19.
+//
+
+import SwiftUI
+
+@main
+struct StudyingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
